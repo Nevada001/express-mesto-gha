4b5abdecceaa531676ed2a9b85a508d5bf672d38
Nevada <olis0001@yandex.ru>
2023-10-10 13:10:36 +0300

    hotfix/github tests

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const Users = require('../models/user');
-const Status = require('../utils/statucCodes')
+const Status = require('../utils/statucCodes');
+const {ValidationError, CastError} = require('mongoose').Error;
 module.exports.getUsers = (req, res) => {
    Users.find({})
     .then(users => res.send(users))
@@ -18,7 +19,10 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   Users.create({name, about, avatar})
   .then(users => res.send(users))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' })
+  .catch((err) => { if (err instanceof ValidationError) {
+    return res.status(Status.BAD_REQUEST).send({message: 'Ошибка валидации полей'})
+  } res.status(500).send({ message: 'Произошла ошибка' })
+}
   )
 }
 
@@ -26,10 +30,20 @@ module.exports.updateUser = (req, res) => {
   const {name, about} = req.body;
   Users.findByIdAndUpdate(req.user._id, {name, about})
   .then(users => res.send(users))
+  .catch((err) => { if (err instanceof ValidationError) {
+    return res.status(Status.BAD_REQUEST).send({message: 'Ошибка валидации полей'})
+  } res.status(500).send({ message: 'Произошла ошибка' })
+}
+  )
 }
 
 module.exports.updateAvatar = (req, res) => {
   const {avatar} = req.body;
   Users.findByIdAndUpdate(req.user._id, {avatar})
   .then(users => res.send(users))
+  .catch((err) => { if (err instanceof ValidationError) {
+    return res.status(Status.BAD_REQUEST).send({message: 'Ошибка валидации полей'})
+  } res.status(500).send({ message: 'Произошла ошибка' })
+}
+  )
 }
