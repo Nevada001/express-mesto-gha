d4dd109e73a4d73403017c3832fa86a9894e663e
Nevada <olis0001@yandex.ru>
2023-10-20 18:18:36 +0300

    3

--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default:
       "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
+    validate: {
+      validator: validator.isURL,
+      message: "Указан неккоректный URL",
+    },
   },
   email: {
     type: String,
